5f4109d1a595d4be7451aad15d60f0ae899e757e
Clopro <cloeberthelin1996@gmail.com>
2021-11-23 11:35:39 +0100

    replace spaces by tab

--- bailleul-berthelin_python/main.py
+++ bailleul-berthelin_python/main.py
@@ -9,103 +9,103 @@ LIEN = "https://public.opendatasoft.com/api/records/1.0/search/?dataset=liste-de
 
 
 def getcheminrelatif():
-    try:
-        with open('/Users/cloeberthelin/labo_school/bailleul-berthelin_python/bailleul-berthelin_python/pourcent_defavorise.csv', 'w'):
-            cheminrelatif = "/Users/cloeberthelin/labo_school/bailleul-berthelin_python"
-    except:
-        cheminrelatif = "C:/Users/VALENTIN/Desktop/E3/python/bailleul-berthelin_python"
-    return cheminrelatif
+	try:
+		with open('/Users/cloeberthelin/labo_school/bailleul-berthelin_python/bailleul-berthelin_python/pourcent_defavorise.csv', 'w'):
+			cheminrelatif = "/Users/cloeberthelin/labo_school/bailleul-berthelin_python"
+	except:
+		cheminrelatif = "C:/Users/VALENTIN/Desktop/E3/python/bailleul-berthelin_python"
+	return cheminrelatif
 
 
 CHEMIN_RELATIF = getcheminrelatif()
 
 
 def remplir_dict_avec_villes(dep_dict, data_utile, nb_villes):
-    """
-        Exemple :
-        dd == {'1' : ['ville1', 'ville2']}
-        dd['1']
-        Renvoie ['ville1', 'ville2']
+	"""
+		Exemple :
+		dd == {'1' : ['ville1', 'ville2']}
+		dd['1']
+		Renvoie ['ville1', 'ville2']
 
-        dd['1'].append("ville3")
-        dd['1']
-        Renvoie ['ville1', 'ville2', 'ville3']
-    """
-    # de 0 à n-1  # Remplacer par nb_villes si différent de nb_villes
-    for i in range(nb_villes):
-        codecommune = data_utile[i]["fields"]["code_commune"]
-        nomcommune = data_utile[i]["fields"]["nom_commune"]
-        codedepartement = codecommune[0:2]
+		dd['1'].append("ville3")
+		dd['1']
+		Renvoie ['ville1', 'ville2', 'ville3']
+	"""
+	# de 0 à n-1  # Remplacer par nb_villes si différent de nb_villes
+	for i in range(nb_villes):
+		codecommune = data_utile[i]["fields"]["code_commune"]
+		nomcommune = data_utile[i]["fields"]["nom_commune"]
+		codedepartement = codecommune[0:2]
 
-        # Ajoute la ville dans la liste associée au département
-        try:
-            # Métropole
-            if codedepartement != '97':
-                dep_dict[codedepartement].append(nomcommune)
-                continue
+		# Ajoute la ville dans la liste associée au département
+		try:
+			# Métropole
+			if codedepartement != '97':
+				dep_dict[codedepartement].append(nomcommune)
+				continue
 
-            # Outre-Mer
-            codeoutremer = codecommune[0:3]
-            dep_dict[codeoutremer].append(nomcommune)
+			# Outre-Mer
+			codeoutremer = codecommune[0:3]
+			dep_dict[codeoutremer].append(nomcommune)
 
-        # Si le code département n'a pas de sens
-        except KeyError:
-            print("\n\n\nBug ici\n\n\n")
-            breakpoint()
+		# Si le code département n'a pas de sens
+		except KeyError:
+			print("\n\n\nBug ici\n\n\n")
+			breakpoint()
 
-    """# tests pour dep sans ville:
+	"""# tests pour dep sans ville:
 	for dep in list(dep_dict):
 		if len(dep_dict[dep]) == 0:
 			dep_dict.pop(dep)
 			# breakpoint()"""
 
-    return dep_dict
+	return dep_dict
 
 
 def pourcent_ville_defavorisee_par_dep(dep_dict):
-    """
+	"""
 
-    """
-    pourcent_dict = dict()
+	"""
+	pourcent_dict = dict()
 
-    for numero_dep in dep_dict.keys():
-        nb_defa = len(dep_dict[numero_dep])
-        nb_comm = nbCparD[numero_dep]
-        pourcent_dict[numero_dep] = round(
-            float(nb_defa)*100 / float(nb_comm), 2)
+	for numero_dep in dep_dict.keys():
+		nb_defa = len(dep_dict[numero_dep])
+		nb_comm = nbCparD[numero_dep]
+		pourcent_dict[numero_dep] = round(
+			float(nb_defa)*100 / float(nb_comm), 2)
 
-    return pourcent_dict
+	return pourcent_dict
 
 
 def create_csv_file(pourcent_defavorise):
-    with open(CHEMIN_RELATIF+'/bailleul-berthelin_python/pourcent_defavorise.csv', 'w', newline='') as csv_file:
-        writer = csv.writer(csv_file)
-        writer.writerow(
-            ['Departement', 'Pourcentage communes defavorisees'])
-        for k, v in pourcent_defavorise.items():
-            writer.writerow([k, v])
-    print(f'Ecriture terminée')
+	with open(CHEMIN_RELATIF+'/bailleul-berthelin_python/pourcent_defavorise.csv', 'w', newline='') as csv_file:
+		writer = csv.writer(csv_file)
+		writer.writerow(
+			['Departement', 'Pourcentage communes defavorisees'])
+		for k, v in pourcent_defavorise.items():
+			writer.writerow([k, v])
+	print(f'Ecriture terminée')
 
 
 def pourcentage_de_communes_défa_par_dép_selon_range_0_25_50_75_100():
-    """
-    y= pourcentage de communes défa par dép en fct de x= range 0/25/50/75/100
-    """
+	"""
+	y= pourcentage de communes défa par dép en fct de x= range 0/25/50/75/100
+	"""
 
 
 def main():
-    json_brut = json.loads(requests.get(LIEN).text)
+	json_brut = json.loads(requests.get(LIEN).text)
 
-    # Définition des constantes et déclaration des listes
-    NB_VILLES = json_brut["nhits"]
-    DATA_UTILE = json_brut["records"]
+	# Définition des constantes et déclaration des listes
+	NB_VILLES = json_brut["nhits"]
+	DATA_UTILE = json_brut["records"]
 
-    departement_dictM = remplir_dict_avec_villes(
-        departement_dict, DATA_UTILE, NB_VILLES)
+	departement_dictM = remplir_dict_avec_villes(
+		departement_dict, DATA_UTILE, NB_VILLES)
 
-    pourcent_defavorise = pourcent_ville_defavorisee_par_dep(departement_dictM)
+	pourcent_defavorise = pourcent_ville_defavorisee_par_dep(departement_dictM)
 
-    create_csv_file(pourcent_defavorise)
+	create_csv_file(pourcent_defavorise)
 
 # end main
 
@@ -121,5 +121,5 @@ def main():
 
 # Run
 if __name__ == "__main__":
-    main()
+	main()
 # end
