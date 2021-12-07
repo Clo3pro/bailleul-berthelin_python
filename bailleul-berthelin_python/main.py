fc14df5679b2abe07241dd21451c328b6d9ffd24
Valentin BAILLEUL <bailleuv@lan.esiee.fr>
2021-12-07 16:22:52 +0100

    faut terminer nb_villes_par_annees

--- bailleul-berthelin_python/main.py
+++ bailleul-berthelin_python/main.py
@@ -3,13 +3,13 @@ import requests
 import json
 from requests.models import RequestEncodingMixin
 
-from DICT_DEP import departement_dict
-from NB_COMMUNES_PAR_DEPARTEMENT import nb_communes_par_dep as nbCparD
+from data_init.DICT_DEP import departement_dict
+from data_init.NB_COMMUNES_PAR_DEPARTEMENT import nb_communes_par_dep as nbCparD
 
 LIEN = "https://public.opendatasoft.com/api/records/1.0/search/?dataset=liste-des-communes-classees-en-zones-defavorisees-au-1er-janvier-2017&q=&rows=9336&refine.zone_defavorisee_simple_fr=ZDS"
 
-CHEMIN_ABSOLU = "/Users/cloeberthelin/labo_school/bailleul-berthelin_python/bailleul-berthelin_python/pourcent_defavorise.csv"
-#CHEMIN_ABSOLU = "C:/Users/VALENTIN/Desktop/E3/python/bailleul-berthelin_python/bailleul-berthelin_python/pourcent_defavorise.csv"
+#CHEMIN_ABSOLU = "/Users/cloeberthelin/labo_school/bailleul-berthelin_python/bailleul-berthelin_python/pourcent_defavorise.csv"
+CHEMIN_ABSOLU = "C:/Users/bailleuv/Desktop/deletefile/bailleul-berthelin_python/bailleul-berthelin_python/pourcent_defavorise.csv"
 
 
 def remplir_dict_avec_villes(dep_dict, data_utile, nb_villes):
@@ -56,13 +56,90 @@ def remplir_dict_avec_villes(dep_dict, data_utile, nb_villes):
 
     return dep_dict
 
-# def annees_entreefunction(dep_dict, nb_villes):
-#     for dep in dep_dict.keys():
-#         for ville in dep_dict[dep]:
-#             villeName = dep_dict[dep][ville]
 
-            #breakpoint()
+def create_dict_annees(json_brut, nb_villes):
+    """
+    {ville1 : année, ville2 : année ...}
+    """
+    annees_dict = dict()
+    for ville_index in range(nb_villes):
+        nom_ville = json_brut['records'][ville_index]['fields']['nom_commune']
+        try:
+            if(nom_ville not in annees_dict.keys()):
+                annees_dict[nom_ville] = (json_brut['records'][ville_index]['fields']['date_de_decision_france_4']).split('-')[0]
+            continue
+        except KeyError: # suite
+            try:
+                if(nom_ville not in annees_dict.keys()):
+                    annees_dict[nom_ville] = (json_brut['records'][ville_index]['fields']['date_de_decision_france_3']).split('-')[0]
+                continue
+            except KeyError: # suite
+                try:
+                    if(nom_ville not in annees_dict.keys()):
+                        annees_dict[nom_ville] = (json_brut['records'][ville_index]['fields']['date_de_decision_france_2']).split('-')[0]
+                    continue
+                except KeyError: # suite
+                    try:
+                        if(nom_ville not in annees_dict.keys()):
+                            annees_dict[nom_ville] = (json_brut['records'][ville_index]['fields']['date_de_decision_france_1']).split('-')[0]
+                        continue
+                    except KeyError:
+                        continue
+    #pas besoin d'ajouter dans le dict, pas de date <==> pas de défa, du coup?
+    # except KeyError: # suite
+    # if(nom_ville not in annees_dict.keys()):
+    # annees_dict[nom_ville] = 'NaN'
+    # continue
+    return annees_dict
 
+
+def annees_entreefunction(dep_dict, nb_villes, dict_annees):
+    dict_des_entrees_dans_defa = dict()
+    for nomville in dict_annees.keys():
+        #breakpoint()
+        for dep in dep_dict.keys():
+            if(nomville in dep_dict[dep]):
+                dict_des_entrees_dans_defa[nomville] = dict_annees[nomville]
+                continue
+    return dict_des_entrees_dans_defa
+
+
+
+    # for dep in dep_dict.keys():
+    #     for index_ville in range(len(dep_dict[dep])):
+    #        ville = dep_dict[dep][index_ville]
+
+def nb_villes_par_annees(dict_annees):
+    nb_ville_par_annee = {
+        '1960-1970': 0,
+        '1970-1980': 0,
+        '1980-1990': 0,
+        '1990-2000': 0,
+        '2000-2010': 0,
+        'test': 0
+    }
+
+    for nomville in dict_annees.keys():
+        annee = int(dict_annees[nomville])
+        if 1980 < annee < 1990:
+            nb_ville_par_annee['1980-1990'] += 1
+        
+        else:
+            nb_ville_par_annee['test'] += 1
+            breakpoint()
+        #nb_ville_par_annee[dict_annees[nomville]] += 1
+
+    return nb_ville_par_annee
+
+# def create_csv_annees(annees_entree_dict):
+#     with open("C:/Users/bailleuv/Desktop/deletefile/bailleul-berthelin_python/bailleul-berthelin_python/annees_test.csv", 'w', newline='') as csv_file:
+#         writer = csv.writer(csv_file)
+#         writer.writerow(
+#             ['Departement', 'Pourcentage communes defavorisees', 'Nombre total communes', 'Nombre communes defavorisees'])
+#         for k, v in pourcent_defavorise.items():
+#             #breakpoint()
+#             writer.writerow([k, v, nbCparD[k], int(nbCparD[k]*pourcent_defavorise[k]/100)])
+#     print(f'Ecriture terminée')
 
 
 def pourcent_ville_defavorisee_par_dep(dep_dict):
@@ -89,6 +166,7 @@ def create_csv_file(pourcent_defavorise):
             #breakpoint()
             writer.writerow([k, v, nbCparD[k], int(nbCparD[k]*pourcent_defavorise[k]/100)])
     print(f'Ecriture terminée')
+
 
 
 def pourcentage_de_communes_défa_par_dép_selon_range_0_25_50_75_100():
@@ -124,12 +202,12 @@ def pourcentage_de_communes_défa_par_dép_selon_range_0_25_50_75_100():
 
 
 """
-A FAIRE PLUS TARD (coordonnées GPS)
-# Récupérer les données géographiques des communes via code insee
-for codeinsee in communes_dict.items():
-    print(codeinsee[0])
-    # url_json_dep = "https://geo.api.gouv.fr/communes?code={codeinsee[0]}&fields=nom,code,codesPostaux,codeDepartement,codeRegion,population&format=geojson&geometry=contour"
-    # url_json_dep_truc = json.loads(requests.get(url_json_dep).text)
+    A FAIRE PLUS TARD (coordonnées GPS)
+    # Récupérer les données géographiques des communes via code insee
+    for codeinsee in communes_dict.items():
+        print(codeinsee[0])
+        # url_json_dep = "https://geo.api.gouv.fr/communes?code={codeinsee[0]}&fields=nom,code,codesPostaux,codeDepartement,codeRegion,population&format=geojson&geometry=contour"
+        # url_json_dep_truc = json.loads(requests.get(url_json_dep).text)
 """
 
 
@@ -143,7 +221,13 @@ def main():
     departement_dictM = remplir_dict_avec_villes(
         departement_dict, DATA_UTILE, NB_VILLES)
 
-    #annees_entree = annees_entreefunction(departement_dictM, NB_VILLES)
+
+    dict_annees = create_dict_annees(json_brut, NB_VILLES)
+    annees_entree_dict = annees_entreefunction(departement_dictM, NB_VILLES, dict_annees)
+    nb_villes_annees = nb_villes_par_annees(dict_annees)
+    breakpoint()
+    create_csv_annees(annees_entree_dict)
+
 
     pourcent_defavorise = pourcent_ville_defavorisee_par_dep(departement_dictM)
 
